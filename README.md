56a83508215e359a3bf25bd26737a16916323985
Rogério Fonseca <rogerio@teccross.com.br>
2023-05-13 10:21:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Requirements:
+
+- Go programming language installed on your system.
+- A valid Microsoft Teams webhook URL.
+- The message text to be sent as a command-line argument.
+
+# Example:
+```bash
+go run main.go "Hello from Go!"
+```
+
+
---
+++ teams.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"os"
+)
+
+type MessagePayload struct {
+	Text string `json:"text"`
+}
+
+func main() {
+
+	if os.Getenv("TEAMS_WEBHOOK_URL") == "" {
+		panic("TEAMS_WEBHOOK_URL environment variable is not set")
+	}
+
+	if len(os.Args) < 2 {
+		panic("Please provide the message text as a command-line argument")
+	}
+
+	webhookURL := os.Getenv("TEAMS_WEBHOOK_URL")
+	messageText := os.Args[1]
+
+	payload := MessagePayload{
+		Text: "**[IMPORTANT]** " + messageText,
+	}
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		println("Message sent successfully!")
+	} else {
+		println("Failed to send message:", resp.Status)
+	}
+}
